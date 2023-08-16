aba564a9fc283a6f358d8d71dfc9e5c12df964c3
shloksuman <shloksuman01@gmail.com>
2023-08-16 13:46:02 +0530

    added basics

--- concepts/basics/functions/function.js
+++ concepts/basics/functions/function.js
@@ -22,7 +22,7 @@ const login = function (username, password){
     console.log("username" , username);
 };
 
-login("shlok"); //SO login has become a function , and it can now
+login("height"); //SO login has become a function , and it can now
 //be called and values be passed into it
 
 
@@ -45,4 +45,72 @@ printMessage("calling printmessage and passing sayBye into it as callback", sayB
 printMessage("passing anony" , function (){
     console.log('I am anonymous function');
 });
+
+//-------------------------------------------------
+
+// IMMEDIATELY INVOKED FUNCTION IIFE
+
+(function(){
+    console.log("this is an IIFe");
+})();
+
+//   ( add function   )() ; 
+//   this directly calls the function without invoking anywhere else
+
+
+//--------------------------------------------------
+
+// creating ARROW FUNCTION
+
+const arrowFunction = ()=>{
+    return "hello";
+}
+
+var c = arrowFunction();
+console.log(c);
+
+const calcArea = (height, width)=> {  //THIS FUNCTION
+    return height * width;
+};
+
+const area1 = calcArea(23, 22) ;
+console.log("area " , area1);
+
+const calcArea1 = (height, width)=> height*width ; //AND THIS FUNCTION ARE EXACTLY SAME
+
+const area2 = calcArea1(20,19);
+console.log("area 2 : " , area2) ;
+
+
+//-----------------------------------
+
+// Question 1
+// Print 15 , 16 , 17 tables using function
+// Function name should be generateTable()
+
+
+function generateTable(num){
+    for(let i = 1 ; i<=10 ; i++){
+        console.log(`${num} * ${i} = ${num*i}`);
+    }
+}
+
+const num1 = parseInt(prompt("Enter the value : "));
+
+generateTable(num1);
+generateTable(16);
+generateTable(17);
+
+
+//--------------------------------------
+
+// Question 2 
+// Create a function that calculate area of circle
+// Function name should be calcAreaOfCicle()
+// create another function calcSquare()
+// Pass calcAreaOfCicle function as callback of calcSqaure
+// like - calcSqaure(calcAreaOfCircle)
+// store value of calculated value in result
+//  like - result = calcSqaure(calcAreaOfCircle)
+// then print result;
 
