0cd7383bf21e326875e105a7d51d73135ca44011
shloksuman <shloksuman01@gmail.com>
2023-08-25 20:42:05 +0530

    some added texts

---
+++ concepts/DOM/dom.js
@@ -0,0 +1,13 @@
+
+console.log(document.body.parentNode);
+
+// This will select parent of body 
+// parent of body is html
+
+console.log(document.body.childNodes);
+// NodeList(4) [text, h1, text, script]
+console.log(document.body.childNodes[3]);
+// returns script at index 3
+
+
+
