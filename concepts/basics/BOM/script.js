8ac86a4e0b3fcb0a5af7cb46fc8e817de4d3c226
shloksuman <shloksuman01@gmail.com>
2023-09-03 14:24:24 +0530

    added frontend page

--- concepts/basics/BOM/script.js
+++ concepts/basics/BOM/script.js
@@ -1,1 +1,14 @@
-console.log(window);
+
+function sayHello() {
+
+  console.log("Hello, world!");
+
+}
+
+
+
+let interval = setInterval(sayHello , 500);
+
+setTimeout(()=>{
+  clearInterval(interval) ; // clear the setInterval
+} , 5000);
